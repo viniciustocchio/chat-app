1abd275a856d06f24dba4ade68f177eabae453bb
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-21 07:06:16 +0200

    added backgroud color

--- components/Chat.js
+++ components/Chat.js
@@ -3,14 +3,14 @@ import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 export default class Chat extends React.Component {
   componentDidMount(){
-    let name = this.props.route.params.name;
-    let color = this.props.route.params.color;
-    this.props.navigation.setOptions({ title: name });
-    this.props.navigation.setOptions({ colcolorWrapper: color });
+    
   }
     render() {
+      let color = this.props.route.params.color;
+      let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
     return (
-      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: color }}>
         <Button
           title="Go to Start"
           onPress={() => this.props.navigation.navigate("Start")}
