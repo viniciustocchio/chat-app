657f0d02240e408444b8e0428aa6d6388199260e
vtocchio <80677157+viniciustocchio@users.noreply.github.com>
2022-10-24 18:02:23 +0200

    communication features

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View, Text, Button, TextInput, StyleSheet, Platform, KeyboardAvoidingView  } from 'react-native';
+import { View, StyleSheet, Platform, KeyboardAvoidingView  } from 'react-native';
+import 'react-native-gesture-handler';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
 
 const firebase = require('firebase');
 require('firebase/firestore');
@@ -181,6 +184,28 @@ export default class Chat extends React.Component {
             />
         );
     }
+  }
+
+  renderCustomActions = (props) => {
+    return <CustomActions {...props} />;
+  };
+
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
   }
 
   render() {
---
+++ components/CustomActions.js
@@ -0,0 +1,160 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connectActionSheet } from '@expo/react-native-action-sheet';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+export default class CustomActions extends React.Component {
+
+  imagePicker = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    try {
+      if (status === 'granted') {
+          let result = await ImagePicker.launchImageLibraryAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          }).catch((error) => console.log(error));
+          if (!result.cancelled) {
+              const imageUrl = await this.uploadImageFetch(result.uri);
+              this.props.onSend({ image: imageUrl });
+          }
+      }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
+
+  takePhoto = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    try {
+      if (status === 'granted') {
+          let result = await ImagePicker.launchCameraAsync({
+              mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          }).catch((error) => console.log(error));
+          if (!result.cancelled) {
+              const imageUrl = await this.uploadImageFetch(result.uri);
+              this.props.onSend({ image: imageUrl });
+          }
+      }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
+
+  getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === 'granted') {
+        let result = await Location.getCurrentPositionAsync({})
+        if (result) {
+          this.props.onSend({
+            location: {
+              longitude: result.coords.longitude,
+              latitude: result.coords.latitude,
+            },
+          });
+        }
+      }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
+
+  uploadImageFetch = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', uri, true);
+        xhr.send(null);
+    });
+
+    const imageNameBefore = uri.split('/');
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+    const ref = firebase.storage().ref().child(`images/${imageName}`);
+    const snapshot = await ref.put(blob);
+    blob.close();
+    return await snapshot.ref.getDownloadURL();
+  };
+
+  onActionPress = () => {
+    const options = [
+      'Choose From Library',
+      'Take Picture',
+      'Send Location',
+      'Cancel',
+    ];
+    const cancelButtonIndex = options.length - 1;
+    this.props.showActionSheetWithOptions(
+      {
+          options,
+          cancelButtonIndex,
+      },
+    async (buttonIndex) => {
+      switch (buttonIndex) {
+        case 0:
+          console.log('user wants to pick an image');
+          return this.imagePicker();
+        case 1:
+          console.log('user wants to take a photo');
+          return this.takePhoto();
+        case 2:
+          console.log('user wants to get their location');
+          return this.getLocation();
+      }
+    }
+    );
+  };
+
+  render() {
+      return (
+          <TouchableOpacity
+              accessible={true}
+              accessibilityLabel='More options'
+              accessibilityHint='Send an image or your geolocation.'
+              style={[styles.container]}
+              onPress={this.onActionPress}
+          >
+              <View style={[styles.wrapper, this.props.wrapperStyle]}>
+                  <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+              </View>
+          </TouchableOpacity>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      width: 26,
+      height: 26,
+      marginLeft: 10,
+      marginBottom: 10,
+  },
+  wrapper: {
+      borderRadius: 13,
+      borderColor: '#b2b2b2',
+      borderWidth: 2,
+      flex: 1,
+  },
+iconText: {
+    color: '#b2b2b2',
+    fontWeight: 'bold',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+  },
+});
+
+CustomActions.contextTypes = {
+actionSheet: PropTypes.func,
+};
+
+CustomActions = connectActionSheet(CustomActions);
